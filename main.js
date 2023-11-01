1d6d9f1857b2f782fb4df932cb82e3994e67f344
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-11-01 10:51:46 +0100

    Added footer

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ import { setup } from './src/setup.js'
 
 document.querySelector('#app').innerHTML = `
   <canvas id="canvas1"></canvas>
+  <footer>
+    <p>WASD or arrowkeys to move, mouse to aim, left mouse button or space to shoot, R to reload your weapon <br>
+    Game created by <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> | <a href="https://github.com/Hjalmar-Lundmark/te4-shooter-game">Source code</a>
+    </p>
+  </footer>
 `
 
 setup(document.querySelector('#canvas1'))
